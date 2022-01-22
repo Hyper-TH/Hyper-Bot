949a04509ad2c8e6139c45433a9515581566509b
Hyper-TH <twila.shayne.habab@outlook.com>
2022-01-22 02:22:07 +0000

    second comit

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 
-const { Client, Intents, Message } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Intents, Message, Channel } = require('discord.js');
+const client = new Client({ 
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] , 
+    partials: ['MESSAGE', 'REACTION']
+});
+const PREFIX = "!h";
+
 
 Date.prototype.addDays = function (days) {
     let date = new Date(this.valueOf());
@@ -39,99 +44,218 @@ client.on("ready", () => {
 })
  
 
-client.on("messageCreate", msg => {
+client.on("messageCreate", async (msg) => {
     
-    // Find sum of 2 numbers
-    if (msg.content.startsWith('!h sum')) {
+    // console.log(`[${msg.author.tag}]: ${msg.content} `);
+    // if (msg.author.bot) return;
+    // msg.channel.send('Message Recieved');
 
-        if (msg.author.bot) return;
+    if (msg.content.startsWith(PREFIX)) {
+
+        // ... spreader operator
+        const [CMD_NAME, ...args] = msg.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
         
-        var userInput = msg.content; 
-        const array = userInput.split(' ');
+        console.log(CMD_NAME);
+        console.log(args);
 
-        console.log(array[2], array[3]);
+        if (CMD_NAME === 'kick') {
+            
+            if (!msg.member.hasPermissions('KICK_MEMBERS'))
+                return msg.reply('You do not have permissions to use that command');
+            if (args.length === 0) 
+                return msg.reply('Please provide an ID');
+            
+            const member = msg.guild.members.cache.get(args[0]);
+            if (member) {
+                member
+                    .kick()
+                    .then((member) => msg.channel.send(`${member} was kicked.`))
+                    .catch((err) => msg.channel.send('I do not have permissions to kick that user:('));
+            } else {
+                msg.channel.send('Member not found');
+            }
+            msg.channel.send('Kicked the user');
+
+        // end  kick
+        } else if (CMD_NAME === 'ban') {
+            if (!msg.member.hasPermissions('BAN_MEMBERS'))
+                return msg.reply('You do not have permissions to use that command');
+            if (args.length === 0) return msg.reply('Please provide an ID');
+
+            try {
+                const user = await msg.guild.members.ban(args[0]);
+                msg.channel.send('User was banned successfully');
+                console.log(user);
+            } catch (err) {
+                console.log(err);
+                msg.channel.send('An error occured. Either I do not have permissions or the user was not found')
+            } // end try catch
+
+        // end ban
+        } // else if (CMD_NAME == 'sum') {
+        //     if (args.length === 0) return msg.reply('Please provide the numbers to be added');
+
+        //     try {
+        //         var sum = parseInt(args[0]) + parseInt(args[1]);
+        //         var output = 'Sum of input numbers: ' + sum;
+
+        //         console.log(output);
+        //         msg.reply(output);
+        //     } catch (err) {
+        //         console.log(err);
+        //         msg.channel.send('An error occured, please check console :(');
+        //     }
+
+        // // end sum
+        // }
+    } // end prefix
+
+    // // Find sum of 2 numbers
+    // if (msg.content.startsWith('sum')) {
+
+    //     if (msg.author.bot) return;
         
-        const sum = parseInt(array[2]) + parseInt(array[3]);
+    //     var userInput = msg.content; 
+    //     const array = userInput.split(' ');
 
-        var output = 'Sum of input numbers: ' + sum;
+    //     console.log(array[2], array[3]);
+        
+    //     const sum = parseInt(array[2]) + parseInt(array[3]);
 
-        console.log(output);
-        msg.reply(output);
+    //     var output = 'Sum of input numbers: ' + sum;
 
-    } // end !h sum
+    //     console.log(output);
+    //     msg.reply(output);
+
+    // } // end !h sum
 
     // Find day of the week (MM/DD/YYYY yearsToAdd)
-    if (msg.content.startsWith('!h day?')) {
+    // if (msg.content.startsWith('!h day?')) {
 
-        if (msg.author.bot) return;
+    //     if (msg.author.bot) return;
 
-        var userInput = msg.content;
-        const fullArray = userInput.split(' ');
+    //     var userInput = msg.content;
+    //     const fullArray = userInput.split(' ');
 
-        // Store user date into array
-        var userDateRaw = fullArray[2].split('/');
+    //     // Store user date into array
+    //     var userDateRaw = fullArray[2].split('/');
 
-        // Store amount of years user input
-        var years = parseInt(fullArray[3]);
+    //     // Store amount of years user input
+    //     var years = parseInt(fullArray[3]);
 
-        // Get user's date
-        // Date.parse() is an alternate option to convert the string date. 
-        // It returns a numeric value instead of a date object. 
-        // Hence it will require further processing if you expect a date object.
-        var miliseconds = new Date();
-        // MM/DD/YYYY (also change from / to -, for some reason it goes one day back with /)
-        var miliseconds = Date.parse(userDateRaw[0] + '-' + userDateRaw[1] +  '-' + userDateRaw[2]); 
+    //     // Get user's date
+    //     // Date.parse() is an alternate option to convert the string date. 
+    //     // It returns a numeric value instead of a date object. 
+    //     // Hence it will require further processing if you expect a date object.
+    //     var miliseconds = new Date();
+    //     // MM/DD/YYYY (also change from / to -, for some reason it goes one day back with /)
+    //     var miliseconds = Date.parse(userDateRaw[0] + '-' + userDateRaw[1] +  '-' + userDateRaw[2]); 
 
-        // console.log(miliseconds);
-        userDate = new Date(miliseconds);
-        console.log(userDate); // for some reason displays a day before
+    //     // console.log(miliseconds);
+    //     userDate = new Date(miliseconds);
+    //     console.log(userDate); // for some reason displays a day before
         
-        // userDate = userDate.addDays(1); // Add one day
+    //     // userDate = userDate.addDays(1); // Add one day
 
-        // Declare day month and years
-        var day = new Date();
-        var month = new Date();
-        var year = new Date();
+    //     // Declare day month and years
+    //     var day = new Date();
+    //     var month = new Date();
+    //     var year = new Date();
 
-        // getDay returns day of week (i.e., Sunday = 0)
-        // getDate returns the day of date
-        date = userDate.getDate();
-        day = userDate.getDay();
-        month = userDate.getMonth() + 1; // January at 0
-        year = userDate.getFullYear();
+    //     // getDay returns day of week (i.e., Sunday = 0)
+    //     // getDate returns the day of date
+    //     date = userDate.getDate();
+    //     day = userDate.getDay();
+    //     month = userDate.getMonth() + 1; // January at 0
+    //     year = userDate.getFullYear();
 
-        // Output details of birthday
-        console.log('Day of your birthday: ', date);
-        console.log('Day of the week: ', daysArr[day])
-        console.log('Month of your birthday: ', month);
-        console.log('Year of your birthday: ', year);
+    //     // Output details of birthday
+    //     console.log('Day of your birthday: ', date);
+    //     console.log('Day of the week: ', daysArr[day])
+    //     console.log('Month of your birthday: ', month);
+    //     console.log('Year of your birthday: ', year);
 
-        // Declare this year and next year's dates
-        var thisYearMiliseconds = new Date();
-        var nextYearMiliseconds = new Date();
+    //     // Declare this year and next year's dates
+    //     var thisYearMiliseconds = new Date();
+    //     var nextYearMiliseconds = new Date();
 
-        // MM/DD/YYYY (think of leap years)
-        thisYearMiliseconds = Date.parse(userDateRaw[0] + '-' + userDateRaw[1] +  '-' + today_yyyy); // Get milliseconds of this year
-        nextYearMiliseconds = Date.parse(userDateRaw[0] + '-' + userDateRaw[1] +  '-' + (today_yyyy + years)); // Get milliseconds of next year
+    //     // MM/DD/YYYY (think of leap years)
+    //     thisYearMiliseconds = Date.parse(userDateRaw[0] + '-' + userDateRaw[1] +  '-' + today_yyyy); // Get milliseconds of this year
+    //     nextYearMiliseconds = Date.parse(userDateRaw[0] + '-' + userDateRaw[1] +  '-' + (today_yyyy + years)); // Get milliseconds of next year
 
-        // Get the 1 year miliseconds difference
-        miliDifference = nextYearMiliseconds - thisYearMiliseconds 
+    //     // Get the 1 year miliseconds difference
+    //     miliDifference = nextYearMiliseconds - thisYearMiliseconds 
 
-        // Add it to current year
-        newMiliseconds = thisYearMiliseconds + miliDifference
+    //     // Add it to current year
+    //     newMiliseconds = thisYearMiliseconds + miliDifference
 
-        // Convert to date
-        var result = new Date(newMiliseconds);
+    //     // Convert to date
+    //     var result = new Date(newMiliseconds);
 
-        day = result.getDay();
-        var output = 'Your birthday will be on a ' + daysArr[day];
+    //     day = result.getDay();
+    //     var output = 'Your birthday will be on a ' + daysArr[day];
 
-        console.log(output);
-        msg.reply(output);        
+    //     console.log(output);
+    //     msg.reply(output);        
     
-    } // end !h day?
+    // } // end !h day?
 
-})
+}); // end message
+
+client.on('messageReactionAdd', (reaction, user) => {
+    const { name } = reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === 'message_id_here') {
+        switch (name) {
+
+            // copy into channel and copy emoji (\:apple:)
+            case '🍎':
+                member.roles.add('id_of_role');              
+                break;
+                
+            case '🍌':
+                member.roles.add('id_of_role');
+                break;
+                
+            case '🍇':
+                member.roles.add('id_of_role');
+                break;
+                
+            case '🍑':
+                member.roles.add('id_of_role');              
+                break;
+        }
+    }
+}); // end reaction add
+
+client.on('messageReactionRemove', (reaction, user) => {
+    const { name } = reaction.emoji;
+    const member = reaction.message.guild.members.cache.get(user.id);
+    if (reaction.message.id === 'message_id_here') {
+        switch (name) {
+
+            // copy into channel and copy emoji (\:apple:)
+            case '🍎':
+                member.roles.remove('id_of_role');              
+                break;
+                
+            case '🍌':
+                member.roles.remove('id_of_role');
+                break;
+                
+            case '🍇':
+                member.roles.remove('id_of_role');
+                break;
+                
+            case '🍑':
+                member.roles.remove('id_of_role');              
+                break;
+        }
+    }
+}); // end reaction remove
 
 // This has to be the last line of the file
 client.login(process.env.TOKEN);

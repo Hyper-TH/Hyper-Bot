3d3e843cb005529fe203a70bdae06ddbb51e5b04
Hyper-TH <twila.shayne.habab@outlook.com>
2022-01-24 00:28:07 +0000

    Added basic slash command

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-require('dotenv').config();
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const { Client, Intents, Message, Channel } = require('discord.js');
-const client = new Client({ 
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] , 
+// require('dotenv').config();
+
+// const { Client, Intents, Message, Channel } = require('discord.js');
+const client = new DiscordJS.Client({ 
+    intents: [
+        Intents.FLAGS.GUILDS, 
+        Intents.FLAGS.GUILD_MESSAGES
+    ] , 
     partials: ['MESSAGE', 'REACTION']
-});
+}); 
 const PREFIX = "!h "; // add space
 
 
@@ -41,8 +48,72 @@ console.log('Today\'s Date: ', today);
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
+
+    // test guild 
+    const guildId = '932730439452471296'
+    const guild = client.guilds.cache.get(guildId)
+    let commands 
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong.',
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+               name: 'num1',
+               description: 'The first numbers.',
+               required: true,
+               type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER 
+            },
+            {
+                name: 'num2',
+                description: 'The second number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER 
+            },
+        ],
+    })
+    // global
 })
- 
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'pong',
+            // ephemeral: true, // comment to let other users see
+        })
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1') || 0 // default value is 0 (use ! if always expect a value)
+        const num2 = options.getNumber('num2') || 0
+        
+        await interaction.deferReply({
+            ephemeral: true
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 5000))
+
+        await interaction.editReply({
+            content: `The sum is ${num1 + num2}`,
+            // ephemeral: true,
+        })
+    }
+})
 
 client.on("messageCreate", async (msg) => {
     
